a03e7c5de2aae7358e16ac304d698118ffdcc609
ABABA <amena@Amens-MacBook-Air.local>
2025-11-26 11:18:54 +0300

    make it work

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import './globals.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
+export const metadata = {
+  title: 'Hahu Lyrics',
+  description: 'Find lyrics by song or artist, powered by Next.js',
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,7 +14,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9499581176530330"
           crossOrigin="anonymous"></script>
         <style>
-          @import url('https://fonts.googleapis.com/css2?family=Menbere:wght@100..700&display=swap');
         </style>
       </head>
       <body style={{ fontFamily: "Menbere" }}>
